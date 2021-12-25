94ffc4ec443a037e435ba82badc819d4f13e062c
Dongkil Shin <dshin47@uwo.ca>
2021-12-25 16:38:00 -0500

    move private to dev.js

--- index.js
+++ index.js
@@ -1,23 +1,48 @@
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
 
-const url = 'mongodb://localhost:27017/local';
-mongoose.connect(url, { useNewUrlParser: true });
+const { User }  = require('./models/user');
+
+const config = require('./config/key');
+
+
+// const url = 'mongodb://localhost:27017/admin';
+mongoose.connect(config.mongoURI, { useNewUrlParser: true });
 
 const db = mongoose.connection;
 
 db.once('open', _ => {
-  console.log('Database connected:', url)
+//   console.log('Database connected:', url)
+  console.log('Database connected:', config.mongoURI)
+
 });
 
 db.on('error', err => {
   console.error('connection error:', err)
 });
 
-app.get('/', (req, res) => {
-    res.send('hello world');
-});
+app.use(bodyParser.urlencoded({ extended: true}));
+app.use(bodyParser.json());
+app.use(cookieParser());
+
+app.post('/api/users/register', (req, res) => {
+    const user = new User(req.body)
+
+    user.save((err, userInfo) => {
+        if (err) return res.json({ success: false, err})
+        return res.status(200).json({
+            success: true
+        })
+    })
+})
+
+
+app.get('/', (req, res) => res.send('hello world1'));
+
+app.get('/api/hello', (req, res) => res.send('hello world2'));
 
 const port = 3003;
 
